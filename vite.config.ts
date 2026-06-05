bf5e6b233506efc0ab1da104d0d75515fb7ed913
2303A51075 <2303a51075@sru.edu.in>
2026-06-05 23:35:38 +0530

    Remove GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/infinix-events/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
